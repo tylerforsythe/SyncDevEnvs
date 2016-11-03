31903ba0e10721c78ab3213166d71b4fbf83324f
Tyler Forsythe <forsythe.tyler@gmail.com>
2016-11-02 23:56:38 -0600

    Change to overlord script to cut compression to just "archive" level (since SQL already does compression and this should only be going over a local network anyway). Testing showed that mx7 saved ~200 MB but took ~15-20 minutes to compress on laptop. Not worth it when on a wired gigabit network. mx1 ran significantly faster (less than 5 minutes). Only reason we're doing it at all is to make sure the file encrypts. It _should_ only be transferring locally, but given that it's on BT Sync, there is potential for it to go across the public internet and even an open wifi.

--- SyncDevEnvs/CommandScripts/OverlordCommands.BLACKBELT.bat
+++ SyncDevEnvs/CommandScripts/OverlordCommands.BLACKBELT.bat
@@ -3,7 +3,7 @@ del /Q "D:\Sync\BT-GT\SyncDevEnvs_SYNC\STATUS_{DATE}.txt"
 
 "C:\Program Files\Microsoft SQL Server\110\Tools\Binn\SQLCMD.EXE" -S BLACKBELT -d IMPrintingLucy -Q "BACKUP DATABASE IMPrintingLucy TO DISK = 'D:\temp\SyncDevEnvs_TEMP\IMPrinting_{DATETIME}.BAK'"
 
-"C:\Program Files\7-Zip\7z" a -mmt -t7z -mx7 "D:\Sync\BT-GT\SyncDevEnvs_SYNC\IMPrinting_{DATETIME}.7z" D:\temp\SyncDevEnvs_TEMP\IMPrinting_{DATETIME}.BAK -pNWSpOspawxHjTkc6FOByjtjky9RP6CSmc96J7qtflxf8qhv1qfANogDLXRss3A9U1mFG9V8DmVrhaYc5qjlyTp93MG4TKe2DxxOcwJ4zO6NYMqXOmcn1OWqsg9mn60o35OnWQmBBaF4SvJu66usx6nfzMIBCm4nCNuDvsaSFqyw6ijptT99QLIeny7g3nu0aMmSY6OGpq3Qc9aFApJBecjf8fmmQozFg5Gxz7hzijRGpM8UWuWTZp13ctKGkZxzYb5KfwkbBaVRS3SyeTi34VEZvxvHcTRONIBryhZ2aYPIwbuAHlMWzubfnTk
+"C:\Program Files\7-Zip\7z" a -mmt -t7z -mx1 "D:\Sync\BT-GT\SyncDevEnvs_SYNC\IMPrinting_{DATETIME}.7z" D:\temp\SyncDevEnvs_TEMP\IMPrinting_{DATETIME}.BAK -pNWSpOspawxHjTkc6FOByjtjky9RP6CSmc96J7qtflxf8qhv1qfANogDLXRss3A9U1mFG9V8DmVrhaYc5qjlyTp93MG4TKe2DxxOcwJ4zO6NYMqXOmcn1OWqsg9mn60o35OnWQmBBaF4SvJu66usx6nfzMIBCm4nCNuDvsaSFqyw6ijptT99QLIeny7g3nu0aMmSY6OGpq3Qc9aFApJBecjf8fmmQozFg5Gxz7hzijRGpM8UWuWTZp13ctKGkZxzYb5KfwkbBaVRS3SyeTi34VEZvxvHcTRONIBryhZ2aYPIwbuAHlMWzubfnTk
 
 @echo COMPLETE {DATETIME}> "D:\Sync\BT-GT\SyncDevEnvs_SYNC\STATUS_{DATE}.txt"
 
